9148e8d2db85ca6cfc1f0560149aefa909a8e0b9
Daphne Avril <daphneavril@outlook.com>
2022-01-31 23:25:02 +0200

    Limiting Middleware Execution to POST Requests

--- node-js-the-complete-guide/working-with-express-js/app.js
+++ node-js-the-complete-guide/working-with-express-js/app.js
@@ -1,19 +1,20 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use('/', (req,res,next) => {
-//     console.log('this always runs');
-//     next();
-// });
-// app.use('/add-product', (req,res,next) => {
-//     console.log('in the another middleware');
-//     res.send('<h1>The add product!</h1>');
-// });
-app.use('/', (req,res,next) => {
-    console.log('in the another middleware');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
