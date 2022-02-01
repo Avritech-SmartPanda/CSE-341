88dc552b0fc14a7dbd93eb336fe72653c7e5a846
Daphne Avril <daphneavril@outlook.com>
2022-02-01 09:31:51 +0200

    Adding a 404 Error Page

--- node-js-the-complete-guide/working-with-express-js/app.js
+++ node-js-the-complete-guide/working-with-express-js/app.js
@@ -1,14 +1,17 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(adminRoute);
+app.use(shopRoute);
+
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
 app.listen(3000);
